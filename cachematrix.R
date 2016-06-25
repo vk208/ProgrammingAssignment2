98bf13c81365da5a46ddd86ae4589a311e158a75
vk208 <vyshi19@gmail.com>
2016-06-25 14:13:52 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,7 @@
 ## inv = where inverse is stored
 ## getinverse = returns the inverse from inv
 ## setinverse = store value of input into inv 
-
 ## makeCacheMatrix makes a special matrix so that an inverse can be taken. 
-
 makeCacheMatrix <- function(x = matrix()) {
       inv = NULL
       set = function(y) {
@@ -19,9 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
            setinverse = setinverse,
            getinverse = getinverse)
 }
-
 ## cacheSolve solves the "matrix" that was created from makeCacheMatrix. If an inverse is already in the cache, it would find it and not recalculate it. 
-
 cacheSolve <- function(x, ...) {
       inv = x$getinverse()
       if(!is.null(inv)) {
